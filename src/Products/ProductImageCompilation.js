2fb9080f396573a1c5010c14980574bc54be9802
Donavyn <donavynpence@yahoo.com>
2023-03-18 15:50:44 -0600

    images acquired and put into 1 folder

---
+++ src/Products/ProductImageCompilation.js
@@ -0,0 +1,11 @@
+import deoderant from './deoderant.png';
+import couriers from './couriers.png';
+import penguin from './penguin.png';
+import toiletPaper from './toiletPaper.png';
+
+
+export {
+    deoderant,
+    couriers,
+    penguin,
+    toiletPaper}
