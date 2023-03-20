01e6a836e21903981cb85137e567e1eaab4ac6cd
Donavyn <donavynpence@yahoo.com>
2023-03-20 09:13:44 -0600

    basic home page complete

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,8 +2,15 @@ import React, {useState} from 'react';
 
 function HomePage({productList}) {
   return (
-    <div>
-        {<img src={productList.item1.img} alt = 'deoderant'/>}
+    <div className='home-page-info'>
+        <h1 className='header'>Welcome to our store!</h1>
+        <p className='mission-statement'>
+            &emsp;&emsp;Here at "Mega Super Corp" we are proud <br/>
+            to have all any HumAn could ever realistically <br/>
+            need! if your needs arent met we will question your <br/>
+            ability to distinguish needs from wants and reality <br/>
+            from fiction haAhHaaaa.
+        </p>
     </div>
   );
 }
