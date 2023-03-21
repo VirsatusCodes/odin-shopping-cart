6745877bb1f4d5176cbab8a218ba8fcba37ffca9
Donavyn <donavynpence@yahoo.com>
2023-03-21 08:17:58 -0600

    add uniqid for mapping

--- src/products/productList.js
+++ src/products/productList.js
@@ -3,34 +3,36 @@ import {deoderant,
         penguin,
         toiletPaper} from './productImageCompilation'
 
+import uniqid from 'uniqid'
+
 const productList = {
   item1 : {
     name: 'Deoderant',
     price: 1.25,
     quantity: 1,
     img: deoderant,
-    id: "ex",
+    id: uniqid(),
   },
   item2 : {
     name: 'Couriers',
     price: 5,
     quantity: 1,
     img: couriers,
-    id: "ex",
+    id: uniqid(),
   },
   item3 : {
     name: 'Penguin',
     price: 5,
     quantity: 1,
     img: penguin,
-    id: "ex",
+    id: uniqid(),
   },
   item4 : {
     name: 'Toilet Paper',
     price: 10,
     quantity: 16,
     img: toiletPaper,
-    id: "ex",
+    id: uniqid(),
   },
 }
 
